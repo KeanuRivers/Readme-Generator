f9d420cf54b1938de0a4650759e7db341d384274
Keanu Rivers <keanurivers404@gmail.com>
2022-08-20 17:44:46 -0700

    added axios & fs pkgs, imported to index.js

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Readme-Generator
+
+    # About The Author
+    # Name: Keanu Rivers
+    ## Username: KeanuRivers
+    ### Location: null
+    
+    First Header | Second Header
+    -------------|---------------
+    Project      | Readme-generator
+    Project URL  | https://github.com/KeanuRivers/Readme-generator
+    Description  | Auto creates a comprehensive Readme file
+    Installation | npm install
+    License      | ISC
+    Test         | npm test
+    
+    
+    ![GitHub Logo](https://avatars.githubusercontent.com/u/106979893?v=4)
+    
+    [![GitHub license](https://img.shields.io/badge/license-ISC-blue.svg)](https://github.com/KeanuRivers/Readme-generator)
+    
+        
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 import inquirer from "inquirer";
-// var inquirer = require("inquirer")
+import axios from "axios";
+import fs from "fs"
+var Useranswer = ""
     // TODO: Create an array of questions for user input
     inquirer.prompt ([
         {
@@ -52,18 +54,49 @@ import inquirer from "inquirer";
             choices: ["MIT", "ISC", "APACHE 2.0", "GPL 3.0"]
         },
     ])
+    .then((userData) => {
+        Useranswer = userData
+        return axios.get(`https://api.github.com/users/${Useranswer.github}`)
+
+    })
+    .then(function(axiosResponse) {
+        console.log(axiosResponse.data);
+        var readMeString = `
+    # About The Author
+    # Name: ${axiosResponse.data.name}
+    ## Username: ${axiosResponse.data.login}
+    ### Location: ${axiosResponse.data.location}
     
+    First Header | Second Header
+    -------------|---------------
+    Project      | ${Useranswer.Project}
+    Project URL  | https://github.com/${Useranswer.github}/${Useranswer.Project}
+    Description  | ${Useranswer.Description}
+    Installation | ${Useranswer.Installation}
+    License      | ${Useranswer.License}
+    Test         | ${Useranswer.Test}
+    
+    
+    ![GitHub Logo](${axiosResponse.data.avatar_url})
+    
+    [![GitHub license](https://img.shields.io/badge/license-${Useranswer.License}-blue.svg)](https://github.com/${Useranswer.github}/${Useranswer.Project})
+    
+        `
+        console.log(readMeString)
+        
+        fs.writeFileSync("./README.md", readMeString)
+    })
     // function to write README file
-    function writeToFile(fileName, data) {
-    }
+    // function writeToFile(fileName, data) {
+    // }
     // TODO: Create a function to write README file
     // function writeToFile(fileName, data) {}
     
     // function to initialize program
-    function init() {
-    // TODO: Create a function to initialize app
-    // function init() {}
+    // function init() {
+    // // TODO: Create a function to initialize app
+    // // function init() {}
     
-    }
+    // }
     // function call to initialize program
     // Function call to initialize app
